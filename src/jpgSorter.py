b18fd9a3fe32eb49abc9068b9c23b10fe9905945
YisusChrist <alejandro.gonzalezm02@estudiante.uam.es>
2023-06-11 14:21:30 +0200

    Added docstrings and refactored code for readability and maintainability

--- src/jpgSorter.py
+++ src/jpgSorter.py
@@ -4,93 +4,173 @@
 """
 @file     jpgSorter.py
 @date     2023-06-11
-@version  1.1
+@version  1.2
 @license  GNU General Public License v3.0
 @author   Alejandro Gonzalez Momblan (agelrenorenardo@gmail.com)
 @desc     Sorts jpg images in a directory by creation date.
 """
 
-import os.path
 import ntpath
-from time import localtime, strftime, strptime, mktime
+import os.path
 import shutil
+from time import localtime, mktime, strftime, strptime
+from typing import List, Optional, Tuple
+
 import exifread
 
 unknownDateFolderName = "date-unknown"
 
 
-def getMinimumCreationTime(exif_data):
-    creationTime = None
-    dateTime = exif_data.get("DateTime")
-    if dateTime is None:
-        dateTime = exif_data.get("Image DateTime")
-    dateTimeOriginal = exif_data.get("EXIF DateTimeOriginal")
-    dateTimeDigitized = exif_data.get("EXIF DateTimeDigitized")
+def getMinimumCreationTime(exif_data: dict) -> str:
+    """
+    Returns the minimum creation time from the given exif data.
 
-    # 3 different time fields that can be set independently result in 9 if-cases
-    if dateTime is None:
-        if dateTimeOriginal is None:
-            # case 1/9: dateTime, dateTimeOriginal, and dateTimeDigitized = None
-            # case 2/9: dateTime and dateTimeOriginal = None, then use dateTimeDigitized
-            creationTime = dateTimeDigitized
-        else:
-            # case 3/9: dateTime and dateTimeDigitized = None, then use dateTimeOriginal
-            # case 4/9: dateTime = None, prefer dateTimeOriginal over dateTimeDigitized
-            creationTime = dateTimeOriginal
-    else:
-        # case 5-9: when creationTime is set, prefer it over the others
-        creationTime = dateTime
+    Args:
+        exif_data (dict): The exif data.
 
+    Returns:
+        str: The minimum creation time.
+
+    Examples:
+        >>> getMinimumCreationTime({"DateTime": "2021:06:11 12:00:00"})
+        '2021:06:11 12:00:00'
+        >>> getMinimumCreationTime({"Image DateTime": "2021:06:11 12:00:00"})
+        '2021:06:11 12:00:00'
+        >>> getMinimumCreationTime({"EXIF DateTimeOriginal": "2021:06:11 12:00:00"})
+        '2021:06:11 12:00:00'
+        >>> getMinimumCreationTime({"EXIF DateTimeDigitized": "2021:06:11 12:00:00"})
+        '2021:06:11 12:00:00'
+    """
+    assert isinstance(exif_data, dict)
+
+    """
+    3 different time fields that can be set independently result in 9 if-cases:
+      1. when creationTime is set, prefer it over the others
+      2. dateTime = None, prefer dateTimeOriginal over dateTimeDigitized
+      3. dateTime and dateTimeDigitized = None, then use dateTimeOriginal
+      4. dateTime and dateTimeOriginal = None, then use dateTimeDigitized
+      5. dateTime, dateTimeOriginal and dateTimeDigitized = None
+    """
+    creationTime = (
+        exif_data.get("DateTime")
+        or exif_data.get("Image DateTime")
+        or exif_data.get("EXIF DateTimeOriginal")
+        or exif_data.get("EXIF DateTimeDigitized")
+    )
     return creationTime
 
 
-def postprocessImage(images, imageDirectory, fileName):
+def postprocessImage(images: list, imageDirectory: str, fileName: str) -> None:
+    """
+    Adds the image to the list of images to be sorted.
+
+    Args:
+        images (list): The list of images to be sorted.
+        imageDirectory (str): The path of the directory containing the image.
+        fileName (str): The name of the image file.
+
+    Examples:
+        >>> postprocessImage([], "C:\\Users\\User\\Desktop\\folder", "image.jpg")
+    """
     imagePath = os.path.join(imageDirectory, fileName)
-    image = open(imagePath, "rb")
-    creationTime = None
     try:
-        exifTags = exifread.process_file(image, details=False)
-        creationTime = getMinimumCreationTime(exifTags)
-    except:
-        print("invalid exif tags for " + fileName)
+        with open(imagePath, "rb") as image:
+            exifTags = exifread.process_file(image, details=False)
+            creationTime = getMinimumCreationTime(exifTags)
+    except Exception:
+        print("Invalid exif tags for " + fileName)
+        creationTime = None
 
     # distinct different time types
     if creationTime is None:
         creationTime = localtime(os.path.getctime(imagePath))
     else:
         try:
-            creationTime = strptime(str(creationTime), "%Y:%m:%d %H:%M:%S")
-        except:
+            creationTime = strptime(creationTime, "%Y:%m:%d %H:%M:%S")
+        except Exception:
             creationTime = localtime(os.path.getctime(imagePath))
 
     images.append((mktime(creationTime), imagePath))
-    image.close()
 
 
-# Creates the requested path recursively.
-def createPath(newPath):
-    if not os.path.exists(newPath):
-        os.makedirs(newPath)
+def createPath(newPath: str) -> None:
+    """
+    Create a directory path recursively if it doesn't already exist.
+
+    Args:
+        newPath (str): The directory path to create.
+    """
+    os.makedirs(newPath, exist_ok=True)
 
 
-# Pass None for month to create 'year/eventNumber' directories instead of 'year/month/eventNumber'.
-def createNewFolder(destinationRoot, year, month, eventNumber):
-    if month is not None:
-        newPath = os.path.join(destinationRoot, year, month, str(eventNumber))
-    else:
-        newPath = os.path.join(destinationRoot, year, str(eventNumber))
+def createNewFolder(
+    destinationRoot: str, year: str, month: Optional[str], eventNumber: int
+) -> None:
+    """
+    Creates a new folder in the destination root based on the specified year,
+    month, and event number.
+
+    Args:
+        destinationRoot (str): The root directory where the new folder will
+            be created.
+        year (str): The year of the event.
+        month (str or None): The month of the event. Pass None to create
+            'year/eventNumber' directories instead of 'year/month/eventNumber'.
+        eventNumber (int): The event number.
+
+    Example:
+        >>> createNewFolder("/path/to/destination", "2023", "06", 1)
+    """
+    newPath = (
+        os.path.join(destinationRoot, year, month, str(eventNumber))
+        if month
+        else os.path.join(destinationRoot, year, str(eventNumber))
+    )
 
     createPath(newPath)
 
 
-def createUnknownDateFolder(destinationRoot):
+def createUnknownDateFolder(destinationRoot: str) -> None:
+    """
+    Creates a folder with an unknown date in the destination root.
+
+    Args:
+        destinationRoot (str): The root directory where the unknown date
+            folder will be created.
+
+    Example:
+        >>> createUnknownDateFolder("/path/to/destination")
+    """
     path = os.path.join(destinationRoot, unknownDateFolderName)
     createPath(path)
 
 
 def writeImages(
-    images, destinationRoot, minEventDeltaDays, splitByMonth=False
-):
+    images: List[Tuple[float, str]],
+    destinationRoot: str,
+    minEventDeltaDays: int,
+    splitByMonth: bool = False,
+) -> None:
+    """
+    Writes the images to the appropriate destination folders based on their
+    creation dates.
+
+    Args:
+        images (List[Tuple[float, str]]): The list of image tuples containing
+            creation time and file path.
+        destinationRoot (str): The root directory where the images will be
+            written.
+        minEventDeltaDays (int): The minimum event delta in days.
+        splitByMonth (bool, optional): Whether to split the destination
+            folders by month. Default is False.
+
+    Example:
+        >>> images = [
+        >>>     (1623384000.0, "/path/to/image1.jpg"),
+        >>>     (1623470400.0, "/path/to/image2.jpg"),
+        >>> ]
+        >>> writeImages(images, "/path/to/destination", 7, splitByMonth=True)
+    """
     minEventDelta = minEventDeltaDays * 60 * 60 * 24  # convert in seconds
     sortedImages = sorted(images)
     previousTime = None
@@ -103,20 +183,23 @@ def writeImages(
         destinationFilePath = ""
         t = localtime(imageTuple[0])
         year = strftime("%Y", t)
-        month = splitByMonth and strftime("%m", t) or None
+        month = strftime("%m", t) if splitByMonth else None
         creationDate = strftime("%d/%m/%Y", t)
         fileName = ntpath.basename(imageTuple[1])
 
         if creationDate == today:
+            # Create a folder for images with an unknown date
             createUnknownDateFolder(destinationRoot)
             destination = os.path.join(destinationRoot, unknownDateFolderName)
             destinationFilePath = os.path.join(destination, fileName)
 
         else:
-            if (previousTime == None) or (
-                (previousTime + minEventDelta) < imageTuple[0]
+            if (
+                previousTime is None
+                or (previousTime + minEventDelta) < imageTuple[0]
             ):
-                eventNumber = eventNumber + 1
+                # If the time difference exceeds the minimum event delta, create a new folder
+                eventNumber += 1
                 createNewFolder(destinationRoot, year, month, eventNumber)
 
             previousTime = imageTuple[0]
@@ -125,23 +208,37 @@ def writeImages(
             destComponents = [v for v in destComponents if v is not None]
             destination = os.path.join(*destComponents)
 
-            # it may be possible that an event covers 2 years.
-            # in such a case put all the images to the event in the old year
-            if not (os.path.exists(destination)):
+            if not os.path.exists(destination):
+                # If the destination folder does not exist, fallback to the previous destination
                 destination = previousDestination
-                # destination = os.path.join(destinationRoot, str(int(year) - 1), str(eventNumber))
 
             previousDestination = destination
             destinationFilePath = os.path.join(destination, fileName)
 
-        if not (os.path.exists(destinationFilePath)):
+        if not os.path.exists(destinationFilePath):
+            # Move the image to the destination folder
             shutil.move(imageTuple[1], destination)
         else:
             if os.path.exists(imageTuple[1]):
+                # Remove the image if it already exists in the destination folder
                 os.remove(imageTuple[1])
 
 
-def postprocessImages(imageDirectory, minEventDeltaDays, splitByMonth):
+def postprocessImages(
+    imageDirectory: str, minEventDeltaDays: int, splitByMonth: bool
+) -> None:
+    """
+    Post-processes the images in the specified directory by organizing them
+    into destination folders.
+
+    Args:
+        imageDirectory (str): The directory containing the images.
+        minEventDeltaDays (int): The minimum event delta in days.
+        splitByMonth (bool): Whether to split the destination folders by month.
+
+    Example:
+        >>> postprocessImages("/path/to/images", 7, splitByMonth=True)
+    """
     images = []
     for root, dirs, files in os.walk(imageDirectory):
         for file in files:
--- src/numberOfFilesPerFolderLimiter.py
+++ src/numberOfFilesPerFolderLimiter.py
@@ -4,39 +4,102 @@
 """
 @file     numberOfFilesPerFolderLimiter.py
 @date     2023-06-11
-@version  1.1
+@version  1.2
 @license  GNU General Public License v3.0
 @author   Alejandro Gonzalez Momblan (agelrenorenardo@gmail.com)
 @desc     Limits the number of files per folder in a directory.
 """
 
-import sys
-import math
 import os
 import shutil
 
 
-def limitFilesPerFolder(folder, maxNumberOfFilesPerFolder):
+def distributeFilesIntoSubfolders(
+    dirPath: str, maxNumberOfFilesPerFolder: int
+) -> None:
+    """
+    Distributes files into subfolders based on the maximum number of files per
+    folder.
+
+    Args:
+        dirPath (str): The path of the directory.
+        maxNumberOfFilesPerFolder (int): The maximum number of files per
+            folder.
+    """
+    numberOfSubfolders = (
+        len(os.listdir(dirPath)) - 1
+    ) // maxNumberOfFilesPerFolder + 1
+    createSubfolders(dirPath, numberOfSubfolders)
+    moveFilesToSubfolders(dirPath, maxNumberOfFilesPerFolder)
+
+
+def createSubfolders(dirPath: str, numberOfSubfolders: int) -> None:
+    """
+    Creates subfolders in the directory.
+
+    Args:
+        dirPath (str): The path of the directory.
+        numberOfSubfolders (int): The number of subfolders to create.
+    """
+    for subFolderNumber in range(1, numberOfSubfolders + 1):
+        subFolderPath = os.path.join(dirPath, str(subFolderNumber))
+        os.makedirs(subFolderPath, exist_ok=True)
+
+
+def moveFilesToSubfolders(
+    dirPath: str, maxNumberOfFilesPerFolder: int
+) -> None:
+    """
+    Moves files to the appropriate subfolders based on the maximum number of
+    files per folder.
+
+    Args:
+        dirPath (str): The path of the directory.
+        maxNumberOfFilesPerFolder (int): The maximum number of files per
+            folder.
+    """
+    fileCounter = 1
+    for file in sorted(os.listdir(dirPath)):
+        source = os.path.join(dirPath, file)
+        if os.path.isfile(source):
+            destDir = str((fileCounter - 1) // maxNumberOfFilesPerFolder + 1)
+            destination = os.path.join(dirPath, destDir, file)
+            shutil.move(source, destination)
+            fileCounter += 1
+
+
+def limitFilesPerFolder(folder: str, maxNumberOfFilesPerFolder: int) -> None:
+    """
+    Limits the number of files per folder in a directory.
+
+    Args:
+        folder (str): The path of the folder to limit.
+        maxNumberOfFilesPerFolder (int): The maximum number of files per
+            folder.
+
+    Raises:
+        ValueError: If the folder does not exist.
+        ValueError: If the maxNumberOfFilesPerFolder is not a positive integer.
+
+    Examples:
+        >>> limitFilesPerFolder("C:\\Users\\User\\Desktop\\folder", 100)
+    """
+    if not os.path.exists(folder):
+        raise ValueError("The folder does not exist.")
+    if maxNumberOfFilesPerFolder <= 0:
+        raise ValueError(
+            "The maxNumberOfFilesPerFolder is not a positive integer."
+        )
+
+    # Iterate through directories and subdirectories in reverse order
     for root, dirs, files in os.walk(folder, topdown=False):
-        for dir in dirs:
-            dirPath = os.path.join(root, dir)
+        for directory in dirs:
+            dirPath = os.path.join(root, directory)
             filesInFolder = len(os.listdir(dirPath))
+
             if filesInFolder > maxNumberOfFilesPerFolder:
-                numberOfSubfolders = (
-                    (filesInFolder - 1) // maxNumberOfFilesPerFolder
-                ) + 1
-                for subFolderNumber in range(1, numberOfSubfolders + 1):
-                    subFolderPath = os.path.join(dirPath, str(subFolderNumber))
-                    if not os.path.exists(subFolderPath):
-                        os.mkdir(subFolderPath)
-                fileCounter = 1
-                for file in os.listdir(dirPath):
-                    source = os.path.join(dirPath, file)
-                    if os.path.isfile(source):
-                        destDir = str(
-                            ((fileCounter - 1) // maxNumberOfFilesPerFolder)
-                            + 1
-                        )
-                        destination = os.path.join(dirPath, destDir, file)
-                        shutil.move(source, destination)
-                        fileCounter += 1
+                distributeFilesIntoSubfolders(
+                    dirPath, maxNumberOfFilesPerFolder
+                )
+
+    # TODO: Add multiprocessing using Pool().map
